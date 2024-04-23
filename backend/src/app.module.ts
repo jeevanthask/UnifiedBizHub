3e6aa99f7f2a2f7ddc2a0776730ed105a75ec06f
jeevanthask <jeevanthanandika@gmail.com>
2024-04-23 07:49:51 +0530

    env file added

---
+++ backend/src/app.module.ts
@@ -0,0 +1,26 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule } from '@nestjs/config';
+import { EmployeesModule } from './employees/employees.module';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: process.env.DATABASE_USER,
+      password: process.env.DATABASE_PASSWORD,
+      database: 'sql_hr',
+      entities: [],
+      synchronize: false,
+    }),
+    EmployeesModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <>
+      <div className="row">
+        <div className="col-md-12">
+          <h2>Main App</h2>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
